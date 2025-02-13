ba80e7be5b7e533359e8f67f25f20bd8d90033f8
yuvrajyuvi <yuvrajyuvi0008@gmail.com>
2025-02-13 21:35:47 +0530

    commits.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,16 @@
 package com.driver;
-class Product{
-    public  int product(int x,int y){ return x;}
-    public int product(int x,int y,int z){return x;}
-    public double product(double x,double y){return x;}
 
-}
 public class Main {
-    public static void main(String[] args){
-        Product p = new Product();
-
-        System.out.println(p.product(2,3)) ;
-        System.out.println(p.product(2,3,4)) ;
-        System.out.println( p.product(0.1,0.3)) ;
+    public static class Product{
+        public  int product(int x,int y){ return x*y;}
+        public int product(int x,int y,int z){return x*y*z;}
+        public double product(double x,double y){return x*y;}
+    }
+        public static void main(String[] args){
+            Product p = new Product();
+            System.out.println(p.product(2,3)) ;
+            System.out.println(p.product(2,3,4)) ;
+            System.out.println( p.product(0.1,0.3)) ;
 
 
     }
