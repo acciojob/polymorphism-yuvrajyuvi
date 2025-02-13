2d092583b2d973ec06c703043b080f283cc946d3
yuvrajyuvi <yuvrajyuvi0008@gmail.com>
2025-02-13 20:07:06 +0530

    commit.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,8 @@ package com.driver;
 class Product{
     public  int product(int x,int y){ return x;}
     public int product(int x,int y,int z){return x;}
-    public double product(double x,double y){return x;}
+    public double product(double x,double y){return x;
+    }
 
 }
 public class Main {
